d222e26d298721a327d859eae1bb4694a7343826
Amir <amir@zettabyte.am>
2024-01-11 17:38:17 +0400

    Update getDeposits function

--- parsian_bank/test.php
+++ parsian_bank/test.php
@@ -7,88 +7,112 @@ $data = fread($file, 5000000);
 fclose($file);
 
 //$result = json_decode('{1"test" : "1234"}');
-$result = json_decode($data);
-$statement = [];
-if((json_last_error() === JSON_ERROR_NONE))
+$result = getDeposits($html = $data, $user_id = 1, $banking_id = 1);
+var_dump($result);
+function getDeposits(string $html, $user_id, $banking_id)
 {
-    if($result == null || $result == ""){
-        return false;
-    }
-
-    if(!key_exists('totalRecord',(array) $result)){
-        return false;
-    }
-
-    for($i=0;$i<$result?->totalRecord;$i++)
-    {
-        $row = $result?->rowDtoList[$i];
-        if($row === null || $row === ""){
+    $result = json_decode($html);
+    $statement = [];
+    if ((json_last_error() === JSON_ERROR_NONE)) {
+        if ($result == null || $result == "")
             return false;
-        }
-        $deposit['amount'] = $row?->transferAmount;
-        if($deposit['amount'] > 0){
-            $deposit['date'] = $row?->date;//change to jalali with jdate inputs are like date function
-            $deposit['description'] = $row?->description;
-//            if(str_contains($row['description'],'تراکنش پُل')){
-//                $deposit['serial'] =
-//            }
-            $deposit['serial'] = $row?->referenceNumber; // serial = referenceNumber
-            if(str_contains($row?->description,'تراکنش پُل')){
-                preg_match('!کدِ رهگیریِ \d{20,22}!', $row->description, $firstTrackingNumberMatches);
-                preg_match('!\d{20,22}!', $firstTrackingNumberMatches[0], $secondTrackingNumberMatches);
-                preg_match('!IR\d{24}!', $row->description, $shebaMatches);
-                $shebaNumber = $shebaMatches[0];
-                $trackingNumber = $secondTrackingNumberMatches[0];
-                $deposit['flag'] = "bridge";
-                $deposit['peygiri'] = $trackingNumber;
-                $deposit['card_number'] = $shebaNumber; // card number = card number ?? account number ?? sheba
-                $deposit['erja'] = $deposit['peygiri'];
-            }
-            if(str_contains($row->description,'انتقال از کارت')){
-                preg_match('!از کارت \d{16}!', $row->description, $firstCardNumberMatches);
-                preg_match('!\d{16}!', $firstCardNumberMatches[0], $secondCardNumberMatches);
-                preg_match('!شماره پیگیری \d{5,7}!', $row->description, $firstTrackingNumberMatches);
-                preg_match_all('!\d{5,7}!', $firstTrackingNumberMatches[0], $secondTrackingNumberMatches);
 
-                $deposit['flag'] = "card to card";
-                $deposit['peygiri'] = "card to card";
-                $deposit['card_number'] = $secondCardNumberMatches[0]; // card number = card number ?? account number ?? sheba
-                $deposit['erja'] = $deposit['peygiri'];
+        if (!key_exists('totalRecord', (array)$result))
+            return false;
+
+        for ($i = 0; $i < $result?->totalRecord; $i++) {
+            $row = $result?->rowDtoList[$i];
+            if ($row === null || $row === "") {
+                return false;
             }
-            if(str_contains($row->description,'انتقالی حساب')){
-                preg_match_all('!\d{14}!', $row->description, $accountMatches);
-                preg_match('!فیش \d{6,8}!', $row->description, $firstReceiptNumberMatches);
-                preg_match('!\d{6,8}!', $firstReceiptNumberMatches[0], $secondReceiptNumberMatches);
+            $amount = $row?->transferAmount;
+            if ($amount > 0) {
+                $date = $row?->date;//change to jalali with jdate inputs are like date function
+                $description = $row?->description;
+                $serial = $row?->referenceNumber;
+                if($serial == '' || $serial == null){
+                    continue;
+                }
+                if (str_contains($row?->description, 'تراکنش پُل')) {
+                    preg_match('!کدِ رهگیریِ \d{20,22}!', $row->description, $firstTrackingNumberMatches);
+                    if(!key_exists(0,$firstTrackingNumberMatches)){
+                            continue;
+                    }
+                    preg_match('!\d{20,22}!', $firstTrackingNumberMatches[0], $secondTrackingNumberMatches);
+                    if(!key_exists(0,$secondTrackingNumberMatches)){
+                        continue;
+                    }
+                    preg_match('!IR\d{24}!', $row->description, $shebaMatches);
+                    if(!key_exists(0,$shebaMatches)){
+                        continue;
+                    }
+                    $shebaNumber = $shebaMatches[0];
+                    $trackingNumber = $secondTrackingNumberMatches[0];
+                    $deposit['flag'] = "bridge";
+                    $peygiri = $erja = $trackingNumber;
+                    $card_number = $shebaNumber; // card number = card number ?? account number ?? sheba
+                }
+                if (str_contains($row->description, 'انتقال از کارت')) {
+                    preg_match('!از کارت \d{16}!', $row->description, $firstCardNumberMatches);
+                    if(!key_exists(0,$firstCardNumberMatches)){
+                        continue;
+                    }
+                    preg_match('!\d{16}!', $firstCardNumberMatches[0], $secondCardNumberMatches);
+                    if(!key_exists(0,$secondCardNumberMatches)){
+                        continue;
+                    }
+                    preg_match('!شماره پیگیری \d{5,7}!', $row->description, $firstTrackingNumberMatches);
+                    if(!key_exists(0,$firstTrackingNumberMatches)){
+                        continue;
+                    }
+                    preg_match('!\d{5,7}!', $firstTrackingNumberMatches[0], $secondTrackingNumberMatches);
+                    if(!key_exists(0,$secondTrackingNumberMatches)){
+                        continue;
+                    }
+
+                    $deposit['flag'] = "card to card";
+                    $peygiri = $erja = $secondTrackingNumberMatches[0];
+                    $card_number = $secondCardNumberMatches[0]; // card number = card number ?? account number ?? sheba
+                }
+                if (str_contains($row->description, 'انتقالی حساب')) {
+                    preg_match('!\d{12,14}!', $row->description, $accountMatches);
+                    if(!key_exists(0,$accountMatches)){
+                        continue;
+                    }
+                    preg_match('!فیش \d{6,8}!', $row->description, $firstReceiptNumberMatches);
+                    if(!key_exists(0,$firstReceiptNumberMatches)){
+                        continue;
+                    }
+                    preg_match('!\d{6,8}!', $firstReceiptNumberMatches[0], $secondReceiptNumberMatches);
+                    if(!key_exists(0,$secondReceiptNumberMatches)){
+                        continue;
+                    }
 //                var_dump($accountMatches);
-                $receiptNumber = $secondReceiptNumberMatches[0];
-                $deposit['flag'] = "account to account";
-                $deposit['peygiri'] = $receiptNumber;
-                $deposit['erja'] = $receiptNumber;
-                $deposit['card_number'] = $accountMatches[0][0]; // card number = card number ?? account number ?? sheba
+                    $receiptNumber = $secondReceiptNumberMatches[0];
+                    $deposit['flag'] = "account to account";
+                    $peygiri = $erja = $receiptNumber;
+                    $card_number = $accountMatches[0]; // card number = card number ?? account number ?? sheba
+                }
+                $statement[] = [
+                    $user_id, // user_id
+                    $banking_id, // banking_id
+                    trim(str_replace(',', '', $amount) ?? ''), // amount
+                    trim($erja ?? ''), // erja
+                    trim($peygiri ?? ''), // peygiri
+                    trim($serial ?? ''), // serial
+                    trim($card_number ?? ''), // card_number
+//                $datetime = str_replace(["‪", "‬"], "", jdate($date)),  // datetime
+//                $bigintDatetime = str_replace(['/', ':', ' '], '', $datetime), // bigint_datetime
+                ];
             }
-//            $deposit['peygiri'] = $row->referenceNumber; // peygiri = peygiri ?? receipt number
-            // if account to account then
-            //  peygiri = receipt number
-            // if bridge then
-            // peygiri = peygiri
-            // if card to card
-            // peygiri = peygiri
-
-            $statement[] = $deposit;
         }
-
-        //->description
-
-//        amount    (OK)
-//        erja      (OK)
-//        peygiri  peygiri or receipt number
-//        serial  erja or serial
-//        card_number  card number or account number or sheba (OK)
-//        datetime (OK)
-//        bigint_datetime (OK)
+    } else {
+        return false;
     }
+    return array_reverse($statement, true);
 }
-var_dump($statement);
+
+//var_dump($statement);
 die;
 //if((json_last_error() === JSON_ERROR_NONE))
 //{
